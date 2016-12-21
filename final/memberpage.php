e9a0de6444d7ec12c85f92fbc9575e76cdc4847a
Josiah Nieves <jtnov20@hotmail.com>
2016-12-21 15:52:38 -0500

    a

--- final/memberpage.php
+++ final/memberpage.php
@@ -17,7 +17,7 @@ require('layout/header.php');
 	    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
 			
 				<h2>Member only page - Welcome <?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES); ?></h2>
-				<p><a class="btn btn-primary btn-block btn-lg" href='logout.php' role="button">Logout</a></p>
+				<p><a style="margin-top: 5em" class="btn btn-primary btn-block btn-lg" href='logout.php' role="button">Logout</a></p>
 				<hr>
 
 		</div>

7e285f126c33e02141cc6d130429991fc7725a4d
Josiah Nieves <jtnov20@hotmail.com>
2016-12-21 17:18:11 -0500

    work

--- final/memberpage.php
+++ final/memberpage.php
@@ -21,7 +21,8 @@ require('layout/header.php');
 
 				<?php
 					echo '<form action="uploadimg.php" method="post" enctype="multipart/form-data">';
-					echo '<input type="image" style="margin-left: auto; margin-right: auto; width: 30em" src="images/' . htmlspecialchars($_SESSION['username'], ENT_QUOTES) . '/profile.jpg" alt="ProfilePic" width="300" height="300"></form>';
+
+					echo '<input type="file" style="margin-left: auto; margin-right: auto; width: 30em" src="images/' . htmlspecialchars($_SESSION['username'], ENT_QUOTES) . '/profile.jpg" alt="ProfilePic" width="300" height="300"></form>';
 					echo '<br><h2 style="text-align: center;">' . htmlspecialchars($_SESSION['username'], ENT_QUOTES) . '<h2>';
 				?>
 
